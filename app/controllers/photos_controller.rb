c02b4429c1202a791fcb79f7a32d513e39efddd3
rradkoff <rradkoff@gmail.com>
2015-10-29 14:42:05 -0500

    Photos controller file code
    added code for 7 menthods within read, create, update and delete
    categories

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,46 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+  def new_form
+  end
+
+  def create_row
+    @photo = Photo.new
+    @photo.caption = params[:caption]
+    @photo.source = params[:source]
+
+    @photo.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+def edit_form
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
+
+def update_row
+    @photo = Photo.find_by({ :id => params[:id]})
+
+    @photo.source = params[:source]
+    @photo.caption = params[:caption]
+
+    @photo.save
+
+    render("show")
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+
+    @photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
 
 end
